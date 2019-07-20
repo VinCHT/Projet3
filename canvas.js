2e38172cbc8c8d49fa7c692a9e39eb941fde151c
Vincent Champagnat <47330327+VinCHT@users.noreply.github.com>
2019-07-20 15:22:45 +0200

    CANVAS
    INDENTATION ok

--- canvas.js
+++ canvas.js
@@ -14,13 +14,17 @@ const ctx = canvas.getContext('2d');
 const penColor = document.querySelector('input[name="penColor"]');
 const penWidth = document.querySelector('input[name="penWidth"]');
 const annuler = document.querySelector('#annuler');
-ctx.strokeStyle = '#000000';
-ctx.lineJoin = 'round';
-ctx.lineCap = 'round';
+const reserver = document.querySelector('btnInsert');
+ctx.strokeStyle = '#000000'; // Pour réaliser le contour
+ctx.lineJoin = 'round'; //détermine la forme utilisée pour joindre deux segments de ligne où ils se rencontrent.
+ctx.lineCap = 'round'; // La propriété lineCap détermine comment les extrêmités de chaque ligne sont dessinées
 ctx.lineWidth = 5;
 
+
+//reserver.addEventListener ('click', reserver);
+//btnInsert.addEventListener('click', secondesEcoulees);
 annuler.addEventListener('click', effacer);
-canvas.addEventListener('mousedown', penDown);
+canvas.addEventListener('mousedown', penDown);  //L'événement mousedown est déclenché sur un élément lorsqu'un bouton de périphérique de pointage est enfoncé alors que le pointeur se trouve à l'intérieur de l'élément.
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', noDown);
 canvas.addEventListener('mouseout', noDown);
@@ -32,11 +36,12 @@ function draw(e) {
     if (!pen.down) return;
     ctx.lineWidth = penWidth.value;
     ctx.strokeStyle = penColor.value;
-    ctx.beginPath();
-    ctx.moveTo(pen.x, pen.y);
-    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.beginPath(); // Appel cette méthode pour créer un nouveau chemin
+    ctx.moveTo(pen.x, pen.y); // moveTo () déplace la fenêtre en cours aux coordonnées spécifiées.
+    ctx.lineTo(e.offsetX, e.offsetY); //Ajoute une ligne droite aux coordonnées (x, y) spécifiées. Mais à tracer avec fill ou stroke
+
     ctx.stroke();
-    [pen.x, pen.y] = [e.offsetX, e.offsetY];
+    [pen.x, pen.y] = [e.offsetX, e.offsetY]; // Provient de MouseEvent.offsetX
 }
 function penDown(e) {
     pen.down = true;
@@ -46,3 +51,10 @@ function penDown(e) {
 function effacer() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
+
+
+
+
+
+
+  
