9e8884e640856ab090c4f1933be55e4852054fd3
Vincent Champagnat <47330327+VinCHT@users.noreply.github.com>
2019-11-27 19:54:49 +0100

    Update canvas.js
    Ajout du canvas POO

--- canvas.js
+++ canvas.js
@@ -1,60 +1,86 @@
-class Crayon {
-    constructor(x, y , down) {
-        this.x=x;
-        this.y=y,
-        this.down=down
+
+class Canvas {
+    constructor(canvas,ctx,penColor,penWidth,down,width,height,annuler,btnReservez, mousePressed ,mouseX, mouseY) {
+        this.canvas= canvas,
+        this.ctx= ctx,
+        this.penColor= penColor,
+        this.penWidth= penWidth,
+        this.down= down,
+        this.width= width,
+        this.height= height,
+        this.annuler =annuler,
+        this.btnReservez=btnReservez,
+        this.mousePressed = mousePressed,
+		this.mouseX = mouseX,
+		this.mouseY = mouseY
+     
     }
-}
+  
+    debutTrait() {
+        this.down = true;
+  
+    }
+  
+    finTrait() {
+        this.down = false;
+        this.ctx.beginPath();
+    }
+  
+    draw(e) {
+        if(!this.down) return;
+        this.ctx.lineWidth = 10;
+        this.ctx.lineCap = "round";
+        this.ctx.lineJoin = 'round';
+        this.ctx.lineWidth = this.penWidth.value;
+        this.ctx.strokeStyle = this.penColor.value;
+        this.ctx.lineTo(e.offsetX, e.offsetY);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(e.offsetX, e.offsetY);
+        this.btnReservez.disabled = false;  
+        
+    }
 
-let pen = new Crayon (0,0,false);
-
-
-const canvas = document.querySelector('#canvas');
-const ctx = canvas.getContext('2d');
-const penColor = document.querySelector('input[name="penColor"]');
-const penWidth = document.querySelector('input[name="penWidth"]');
-const annuler = document.querySelector('#annuler');
-const reserver = document.querySelector('btnInsert');
-ctx.strokeStyle = '#000000'; // Pour réaliser le contour
-ctx.lineJoin = 'round'; //détermine la forme utilisée pour joindre deux segments de ligne où ils se rencontrent.
-ctx.lineCap = 'round'; // La propriété lineCap détermine comment les extrêmités de chaque ligne sont dessinées
-ctx.lineWidth = 5;
-
-
-//reserver.addEventListener ('click', reserver);
-//btnInsert.addEventListener('click', secondesEcoulees);
-annuler.addEventListener('click', effacer);
-canvas.addEventListener('mousedown', penDown);  //L'événement mousedown est déclenché sur un élément lorsqu'un bouton de périphérique de pointage est enfoncé alors que le pointeur se trouve à l'intérieur de l'élément.
-canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mouseup', noDown);
-canvas.addEventListener('mouseout', noDown);
-function noDown() {
-    console.log('out');
-    pen.down = false;
-}
-function draw(e) {
-    if (!pen.down) return;
-    ctx.lineWidth = penWidth.value;
-    ctx.strokeStyle = penColor.value;
-    ctx.beginPath(); // Appel cette méthode pour créer un nouveau chemin
-    ctx.moveTo(pen.x, pen.y); // moveTo () déplace la fenêtre en cours aux coordonnées spécifiées.
-    ctx.lineTo(e.offsetX, e.offsetY); //Ajoute une ligne droite aux coordonnées (x, y) spécifiées. Mais à tracer avec fill ou stroke
-
-    ctx.stroke();
-    [pen.x, pen.y] = [e.offsetX, e.offsetY]; // Provient de MouseEvent.offsetX
-}
-function penDown(e) {
-    pen.down = true;
-    [pen.x, pen.y] = [e.offsetX, e.offsetY];
-    console.log(pen);
-}
-function effacer() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-
-
-
-
-
+    effacer() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.btnReservez.disabled = true;  
+    }
+ 
+    evenement() {
+        this.canvas.addEventListener("mousedown", this.debutTrait.bind(this));
+        this.canvas.addEventListener("mousemove", this.draw.bind(this));
+        this.canvas.addEventListener("mouseup", this.finTrait.bind(this));
+        this.canvas.addEventListener('touchmove', this.onTouchMove.bind(this), false);
+        this.canvas.addEventListener('touchstart', this.onTouchStart.bind(this), false);
+        this.canvas.addEventListener('touchend', this.onMouseUp.bind(this), false);
+    }
+    onTouchMove(event){
+        if (this.mousePressed) {
+            event.preventDefault();
+            this.mouseX = (event.targetTouches[0].pageX) - this.canvas.offsetLeft;
+            this.mouseY = (event.targetTouches[0].pageY) - this.canvas.offsetTop;
+            this.ctx.lineWidth = this.penWidth.value;
+        this.ctx.strokeStyle = this.penColor.value;
+            this.ctx.lineTo(this.mouseX, this.mouseY);
+            this.ctx.stroke();
+            this.btnReservez.disabled = false;  
+        }
+    }
+    
+    
+    onTouchStart(event){
+        this.mousePressed = true;
+        this.mouseX = (event.targetTouches[0].pageX) - this.canvas.offsetLeft;
+        this.mouseY = (event.targetTouches[0].pageY) - this.canvas.offsetTop;
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.mouseX, this.mouseY);
+    }
+  
+    onMouseUp(event){
+        this.mousePressed = false;
+    }
+  }
+  
+//   let monCanvas = new Canvas(document.getElementById('canvas'),this.canvas.getContext('2d'),document.querySelector('input[name="penColor"]'),document.querySelector('input[name="penWidth"]'),false,10,10,document.querySelector('#annuler'),document.getElementById('reserver'),false,0,0);
+//   monCanvas.evenement();
   
